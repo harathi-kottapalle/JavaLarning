f14d526c385c94c5f1e09ac2ee9a3aa095a27170
KottapalleHarathi <kottapalleharathi12@gmail.com>
2025-01-21 16:29:40 +0530

    implimented logic foe even or odd number

---
+++ src/Ternaryoperators/eveno_oddnumber.java
@@ -0,0 +1,10 @@
+package Ternaryoperators;
+
+public class eveno_oddnumber {
+    public static void main(String[] args) {
+        //write a program to print even and odd number//
+        int num=10;
+        String result = num%2==0? "Given number is even number":"given number is odd number";
+        System.out.println(result);
+    }
+}
